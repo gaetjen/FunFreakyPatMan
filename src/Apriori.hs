0052456dea89893b6ff7034baf8eda627523b66c
gaetjen <johannesgaetjen@gmail.com>
2016-06-04 00:55:11 +0200

    Apriori works! next up: write result to file

--- src/Apriori.hs
+++ src/Apriori.hs
@@ -1,6 +1,7 @@
 module Apriori  where
 import FileIO
 import Data.List
+import GHC.Stack
 
 type ItemSet = [String]     -- an item set is a collection of items, which are identified with strings
 type Item = String
@@ -33,30 +34,31 @@ main path file minSupport = do
 
 
 apriori :: [ItemSet] -> Int -> HasseTree
-apriori dataSet minSupport = apriori' dataSet (singletons dataSet minSupport) minSupport 2
+apriori dataSet minSupport = apriori' dataSet (singletons dataSet minSupport) minSupport 1
 
 apriori' :: [ItemSet] -> HasseTree -> Int -> Int -> HasseTree
-apriori' _ [] _ _ = []
-apriori' dataSet hasseTree minSupport k -- k = level of iteration
-    | k > minSupport = hasseTree
-    | otherwise = apriori' dataSet postPruned minSupport (k + 1) where
-        postPruned = (posteriorPrune dataSet aprioriPruned minSupport k)
-        aprioriPruned = (genCandidates hasseTree hasseTree minSupport (k - 1) [])
+-- apriori' _ [] _ _ = []
+apriori' dataSet hasseTree minSupport k
+    | k > (length hasseTree) = hasseTree
+    | otherwise = apriori' dataSet postPruned minSupport (k + 1) where -- k = level of iteration
+        postPruned = (posteriorPrune dataSet aprioriPruned minSupport (k + 1))
+--         postPruned = aprioriPruned
+        aprioriPruned = (genCandidates hasseTree hasseTree k [])
 
 -- generates viable (i.e. according to a priori property) candidates
-genCandidates :: HasseTree -> HasseTree -> Int -> Int -> ItemSet -> HasseTree
-genCandidates [] _ _ _ _ = [] -- stop when we are at end of list
-genCandidates (htH@(HasseLeafNode itm n):htT) reference minSupport k cand -- cand for candidate
+genCandidates :: HasseTree -> HasseTree -> Int -> ItemSet -> HasseTree
+genCandidates [] _ _ _ = [] -- stop when we are at end of list
+genCandidates (htH@(HasseLeafNode itm n):htT) reference k cand -- cand for candidate
     | k == 1 = ((HasseTreeNode itm n (nextLevel (reverse (itm:cand)) htT reference)):nextSameLine) -- front: node with old item and support counter, and pointer to newly generated child node
     | k > 1 = htH:nextSameLine where
-        nextSameLine = (genCandidates htT reference minSupport k cand)
-genCandidates ((HasseTreeNode itm n child):htT) reference minSupport k cand
-    = (HasseTreeNode itm n (genCandidates child reference minSupport (k - 1) (itm:cand))):(genCandidates htT reference minSupport k cand)
+        nextSameLine = (genCandidates htT reference k cand)
+genCandidates ((HasseTreeNode itm n child):htT) reference k cand
+    = (HasseTreeNode itm n (genCandidates child reference (k - 1) (itm:cand))):(genCandidates htT reference k cand)
 
 nextLevel :: ItemSet -> HasseTree -> HasseTree -> HasseTree
 nextLevel _ [] _ = []
 nextLevel cand ((HasseLeafNode itm n):hlT) reference = -- hl for Hasse Leaves
-    case any (==True) $ aprioriPrune (cand ++ [itm]) reference of
+    case any (True==) $ aprioriPrune (cand ++ [itm]) reference of
         True -> nextLevel cand hlT reference
         False -> (HasseLeafNode itm 0):(nextLevel cand hlT reference)
 
@@ -72,17 +74,17 @@ aprioriPrune' prev omit tail@(tailH:tailT) reference = (notSupported (prev ++ ta
 -- gives True when the candidate is not in the HasseTree, i.e. the support is too small
 notSupported :: ItemSet -> HasseTree -> Bool
 notSupported cand [] = True
-notSupported [lst] ((HasseLeafNode itm _):htT)
-    | lst == itm = False
-    | lst < itm = notSupported [lst] htT
+notSupported [lst] (htH:htT)
+    | lst == item htH = False
+    | lst > item htH = notSupported [lst] htT
 notSupported (candH:_) (htH:_)
-    | candH > (item htH) = True -- we've passed the point of no return, there is no way to still find the candidate in the tree
+    | candH < (item htH) = True -- we've passed the point of no return, there is no way to still find the candidate in the tree
 notSupported cand@(candH:_) ((HasseLeafNode itm _):htT)
     | candH == itm = True -- guaranteed that the Leaf Node is not on the last layer, i.e. supersets are infrequent, because of earlier pattern matching
-    | candH < itm = notSupported cand htT -- otherwise iterate through to find the node on which to go down
+    | candH > itm = notSupported cand htT -- otherwise iterate through to find the node on which to go down
 notSupported cand@(candH:candT) ((HasseTreeNode itm _ nxtLevel):htT)
     | candH == itm = notSupported candT nxtLevel -- go down to the next level, omit first element of candidate
-    | candH < itm = notSupported cand htT
+    | candH > itm = notSupported cand htT
 
 posteriorPrune :: [ItemSet] -> HasseTree -> Int -> Int -> HasseTree
 posteriorPrune dataSet hasseTree minSupport k = removeInfrequent minSupport (addDataSet dataSet hasseTree k) k
@@ -91,7 +93,8 @@ posteriorPrune dataSet hasseTree minSupport k = removeInfrequent minSupport (add
 removeInfrequent :: Int -> HasseTree -> Int -> HasseTree
 removeInfrequent _ [] _ = []
 removeInfrequent minSupport ht 1 = filter (\x -> (count x) >= minSupport) ht
-removeInfrequent minSupport (htH:htT) k = (removeInfrequent minSupport (child htH) (k - 1)) ++ (removeInfrequent minSupport htT k)
+removeInfrequent minSupport (htH@(HasseLeafNode _ _):htT) k = htH:(removeInfrequent minSupport htT k)
+removeInfrequent minSupport ((HasseTreeNode itm n child):htT) k = [HasseTreeNode itm n (removeInfrequent minSupport child (k - 1))] ++ (removeInfrequent minSupport htT k)
 
 -- counts and adds all sets of size k
 addDataSet :: [ItemSet] -> HasseTree -> Int -> HasseTree
@@ -103,13 +106,13 @@ addItemSet [] ht _ = ht
 addItemSet _ [] _ = []
 addItemSet is@(isH:isT) ht@(htH:htT) 1
     | isH == item htH = (incCount htH):(addItemSet isT htT 1)
-    | isH < item htH = htH:(addItemSet is htT 1)
-    | isH > item htH = ht
+    | isH > item htH = htH:(addItemSet is htT 1)
+    | isH < item htH = ht
 addItemSet is (htH@(HasseLeafNode _ _):htT) k = htH:(addItemSet is htT k)
 addItemSet is@(isH:isT) ht@(htH@(HasseTreeNode itm n child):htT) k
     | isH == item htH = (HasseTreeNode itm n (addItemSet isT child (k - 1))):(addItemSet isT htT k)
-    | isH < item htH = htH:(addItemSet is htT k)
-    | isH > item htH = ht
+    | isH > item htH = htH:(addItemSet is htT k)
+    | isH < item htH = ht
 
 -- generates the top level of the hasse tree with all the singletons, filtered for minimum support and sorted lexicographically
 singletons :: [ItemSet] -> Int -> HasseTree
